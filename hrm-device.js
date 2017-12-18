6163bc536118d14e4b5fec180ad145ba7cc94b4c
unknown <nkhs@yandex.com>
2017-12-19 02:03:07 +0800

    ! noble-device usage( HRM )

---
+++ hrm-device.js
@@ -0,0 +1,46 @@
+var async = require('async');
+
+var NobleDevice = require('noble-device');
+
+var idOrLocalName = process.argv[2];
+
+if (!idOrLocalName) {
+  console.log("node hrm-device.js [ID or local name]");
+  process.exit(1);
+}
+
+var YOUR_THING_SERVICE_UUID = '1977';
+var YOUR_THING_NOTIFY_CHAR = '1028';
+
+var HRMDevice = function (device) {
+  NobleDevice.call(this, device);
+};
+
+HRMDevice.is = function (device) {
+  var localName = device.advertisement.localName;
+  return (device.id === idOrLocalName || localName === idOrLocalName);
+};
+
+NobleDevice.Util.inherits(HRMDevice, NobleDevice);
+NobleDevice.Util.mixin(HRMDevice, NobleDevice.DeviceInformationService);
+NobleDevice.Util.mixin(HRMDevice, NobleDevice.HeartRateMeasumentService);
+HRMDevice.discover(function (device) {
+  console.log('discovered: ' + device);
+
+  device.on('disconnect', function () {
+    console.log('disconnected!');
+    process.exit(0);
+  });
+
+  device.on('measumentChange', function (data) {
+    console.log("update measument: " + data);
+  });
+
+  device.connectAndSetUp(function (callback) {
+    console.log('connectAndSetUp');
+
+    device.notifyMeasument(function (counter) {
+      console.log('notifyMeasument');
+    });
+  });
+});
