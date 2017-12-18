601f02e76028db01a18f45ff4d9d453986078f44
unknown <nkhs@yandex.com>
2017-12-18 09:41:48 +0800

    ble

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var noble = require('noble');
+
+app.get('/', (req, res) => {
+    noble.on('stateChange', function (state) {
+        if (state === 'poweredOn') {
+            noble.startScanning();
+        } else {
+            noble.stopScanning();
+        }
+    });
+    // res.send('Hello World!');
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
