fbb6887c7d6bdd81fbc8bd3c79c726328a25942f
unknown <nkhs@yandex.com>
2017-12-18 11:05:05 +0800

    u

--- index.js
+++ index.js
@@ -127,8 +127,8 @@ function explore(peripheral) {
                                 var characteristicInfo = '  ' + characteristic.uuid;
                                 if (characteristic.uuid == '1028') {
                                     characteristic.on('data', function (data, isNotification) {
-                                        var a = data.readUInt8(0);
-                                        var b = data.readUInt8(1);
+                                        var a = data.readUInt8(0) & 0x00FF;
+                                        var b = data.readUInt8(1) & 0x00FF;
                                         var ecg = a * 255 + b;
                                         console.log('Ecg : ', ecg);
                                     });
